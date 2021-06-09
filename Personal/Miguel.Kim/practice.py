b62ed54107570d2040618f0315b8a42d1a8d04f5
dohyeon3713 <dohyeon1234@naver.com>
2021-06-09 14:06:48 +0900

    2. 자료형연습
    나도코딩 내용 복습

---
+++ Personal/Miguel.Kim/practice.py
@@ -0,0 +1,37 @@
+#숫자형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+#문자열
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+
+#Boolean 참/거짓
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5>10))
+
+#변수 (애완동물을 소개해 주세요~)
+
+animal = "고양이"
+name = "해피"
+age = 2
+hobby = "공놀이"
+is_adult = age >=3
+
+print("우리집 "+animal+"의 이름은 "+name+"예요")
+#+를 이용하기
+print(name+"는 " +str(age)+"살이며, "+hobby+"을 매우 좋아해요.")
+print(name,"는", age, "살이며, " ,hobby, "을 아주 좋아해요.")
+print(name,"는 어른일까요? "+str(is_adult))
