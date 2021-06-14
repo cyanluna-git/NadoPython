1276437240d10d028aa493cdf959682f7eb52601
hyungmin08 <hyungmin08@naver.com>
2021-06-14 14:05:49 +0900

    practice_#2
    2nd homework

---
+++ Personal/hayden.park/practice_#2.py
@@ -0,0 +1,41 @@
+# # 숫자 자료형
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# # 문자열 자료형
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+# print("ㅋ"*9)
+
+# # Boolean 자료형
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not (5 > 10))
+
+# # 변수
+# # 애완동물을 소개해 주세요
+# animal = "고양이"
+# name = "해피"
+# age = 4
+# hobby = "낮잠"
+# is_adult = age >= 3
+# print("우리집 " + animal + "의 이름은 " + name + "에요")
+# hobby = "공놀이"
+# # print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
+
+# Quiz 1) 변수를 이용하여 다음 문장을 출력하시오
+station = ["사당","신도림","인천공항"]
+for i in station:
+    print(i + "행 열차가 들어오고 있습니다.")
+
