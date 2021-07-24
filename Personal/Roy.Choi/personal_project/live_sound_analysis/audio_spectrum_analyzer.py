42b96f6ef90e98dfb93afb4c724ff6cde737aeaa
roy1117 <xogus1372@gmail.com>
2021-07-25 01:52:37 +0900

    Editted data conversion

--- Personal/Roy.Choi/personal_project/live_sound_analysis/audio_spectrum_analyzer.py
+++ Personal/Roy.Choi/personal_project/live_sound_analysis/audio_spectrum_analyzer.py
@@ -17,16 +17,19 @@ stream = p.open(
     output = True,
     frames_per_buffer = CHUNK
 )
-fig, ax = plt.subplots()
 
-x = np.arange(0, 2*CHUNK, 2)
+
+fig, ax = plt.subplots()
+x = np.arange(0, CHUNK, 1)
 
 line, = ax.plot(x, np.random.rand(CHUNK))
-ax.set_ylim(0, 255)
+ax.set_ylim(-30000 , 30000)
 ax.set_xlim(0, CHUNK)
 while True:
-    data = stream.read(CHUNK)
-    data_int = np.array(struct.unpack(str(2 * CHUNK) + 'B', data), dtype ='b')[::2]+127
+    data = stream.read(CHUNK, exception_on_overflow=False)
+    # pyaudio.paInt16 is signed 16bit binary string ranging from -32768 to 32767
+    # thus used 'h'(unsigned 2 bytes integer) to unpack binary data
+    data_int = np.array(struct.unpack(str(CHUNK) + 'h', data), dtype = 'int16')
     line.set_ydata(data_int)
     plt.pause(0.0001)
 
