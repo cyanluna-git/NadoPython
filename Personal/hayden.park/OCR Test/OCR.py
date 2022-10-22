f6f915e573013c066b6e17a83a682c8149b72da9
hyungmin08 <52654141+hyungmin08@users.noreply.github.com>
2022-10-23 00:06:19 +0900

    OCR Updated
    Updated

--- Personal/hayden.park/OCR Test/OCR.py
+++ Personal/hayden.park/OCR Test/OCR.py
@@ -1,21 +1,24 @@
 from PIL import Image
 from pytesseract import *
 import cv2
-import sys
 import os
 
-def Conversion(ImageFile):
+def Conversion(file):
     multiple = 4
-    image = cv2.resize(cv2.imread(ImageFile),(0,0),fx=multiple, fy=multiple, interpolation=cv2.INTER_LINEAR)
+    image = cv2.resize(cv2.imread(file),(0,0),fx=multiple, fy=multiple, interpolation=cv2.INTER_LINEAR)
     text = image_to_string(image,lang="eng",config='--psm 1 -c preserve_interword_spaces=1')
     return text
 
-def ParseText(text):
-    result = []
-    parse = text.split('\n\n')
-    for item in parse:
-        result.append(item.split('\n'))
-    result = list(map(list, zip(*result)))
+def ParseText(text, cnt_row, cnt_col):
+    text = text.replace('\n\n','\n')
+    list = text.split('\n') 
+    list.remove("")
+    result=[]
+    for i in range(cnt_row):
+        temp=[]
+        for j in range(cnt_col):
+            temp.append(list[i + j*cnt_row])
+        result.append(temp)
     return result
     
 
@@ -31,23 +34,24 @@ if __name__ == '__main__':
         os.environ["PATH"] += os.pathsep + tesseract_home
 
     # Read config file
-    config = this_program_directory + '\Path_Capture_Folder.ini'
+    config = f'{this_program_directory}\\Path_Capture_Folder.ini'
     with open(config,'r') as f:
         folder = f.readline() # 사진파일 저장 폴더, Config.ini 내부에서 지정
         f.close
     fileext = ('.jpg','.JPG','.png','.PNG')
     photos = [os.path.join(folder,file) for file in os.listdir(folder) if file.endswith(fileext)]
-    print(photos)
     
     for photo in photos:
+        print('Converting "{}" in progress...'.format(os.path.basename(photo)))
         result_file = os.path.splitext(photo)[0] + '.csv'
-        result = ParseText(Conversion(photo))
+        result = ParseText(Conversion(photo),5,4)
         r = open(result_file,'w')
         for row in result:
             string = ",".join(row)
             r.write(string + '\n')
-            print(string)
         r.close
+    
+    input('Press Enter to Finish.')
 
 
 
